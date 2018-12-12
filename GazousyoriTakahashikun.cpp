d7813a93dbf02767fdc6059d7d0029d53f675b64
ei <chronolavos0124@gmail.com>
2018-12-12 14:04:59 +0900

    2018_12_12

--- GazousyoriTakahashikun.cpp
+++ GazousyoriTakahashikun.cpp
@@ -1,68 +1,71 @@
-#include <bits/stdc++.h>
-
+#include <bits/stdc++.h> 
+ 
+#define INF (1<<31)//INT_MAX/2
+#define MOD 1000000007
+ 
+using ll  = long long;
+using ull = unsigned long long;
+ 
 int main(int argc, char *argv[])
 {
     int H, W;
     std::cin >> H >> W;
 
-    char S[H+2][W+2];
-    char ans[H+2][W+2];
-    char ans_dum[H+2][W+2];
-    char dum[H][W];
+    std::vector<std::string> S(H);
+    std::vector<std::string> dummy(H);
 
-    for (int i = 0; i < H+2; i++) {
-        for (int j = 0; j < W+2; j++) {
-            S[i][j] = '#';
-        }
+    for (int i = 0; i < H; i++) {
+        std::cin >> S[i];
     }
 
-    for (int i = 1; i <= H; i++) {
-        for (int j = 1; j <= W; j++) {
-            std::cin >> S[i][j];
-        }
-    }
+    dummy = S;
 
-    for (int i = 1; i <= H; i++) {
-        for (int j = 1; j <= W; j++) {
-            dum[i-1][j-1] = S[i][j];
-        }
-    }
-
-    for (int i = 0; i < H+2; i++) {
-        for (int j = 0; j < W+2; j++) {
-            ans[i][j] = S[i][j];
-        }
-    }
-
-    for (int i = 0; i < H+2; i++) {
-        for (int j = 0; j < W+2; j++) {
-            if ((S[i-1][j-1] == '#') && (S[i][j-1] == '#') && (S[i+1][j-1] == '#') && (S[i-1][j] == '#') && (S[i][j] == '#') && (S[i+1][j] == '#') && (S[i-1][j+1] == '#') && (S[i][j+1] == '#') && (S[i+1][j+1] == '#')) {
-                ans[i][j] = '#';
-            } else {
-                ans[i][j] = '.';
-            }
-        }
-    }
-
-    for (int i = 0; i < H+2; i++) {
-        for (int j = 0; j < W+2; j++) {
-            ans_dum[i][j] = ans[i][j];
-        }
-    }
-
-    for (int i = 0; i < H+2; i++) {
-        for (int j = 0; j < W+2; j++) {
-            if (ans[i][j] == '#') {
-                
-            }
-        }
-    }
+    std::vector<std::vector<char>> ans(H, std::vector<char>(W, '.'));
 
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            std::cout << dum[i][j];
+            bool flag = true;
+            for (int y = -1; y <= 1; y++) {
+                for (int x = -1; x <= 1; x++) {
+                    if (0 <= i+y && i+y < H && 0 <= j+x && j+x < W) {
+                        if (S[i+y][j+x] == '.') {
+                            flag = false;
+                        }
+                    }
+                }
+            }
+            if (flag) {
+                ans[i][j] = '#';
+                for (int y = -1; y <= 1; y++) {
+                    for (int x = -1; x <= 1; x++) {
+                        if (0 <= i+y && i+y < H && 0 <= j+x && j+x < W) {
+                            dummy[i+y][j+x] = '.';
+                        }
+                    }
+                }
+            }
         }
-        std::cout << "" << std::endl;
+    }
+
+    bool flag = true;
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
+            if (dummy[i][j] == '#') {
+                flag = false;
+            }
+        }
+    }
+
+    if (!flag) {
+        std::cout << "impossible" << std::endl;
+    } else {
+        std::cout << "possible" << std::endl;
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                std::cout << ans[i][j];
+            }
+            std::cout << std::endl;
+        }
     }
 
     return 0;
