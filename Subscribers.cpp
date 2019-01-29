4d1e6ead2477e9c8bf45bda428f08e6ab93a3b55
ei <chronolavos0124@gmail.com>
2019-01-29 22:53:53 +0900

    2019_01_29

--- Subscribers.cpp
+++ Subscribers.cpp
@@ -3,12 +3,25 @@
 #define INF (1<<31) - 1 //INT_MAX/2
 #define MOD 1000000007
 #define PI  acos(-1)
-
+ 
 using ll  = long long;
 using ull = unsigned long long;
 using P = std::pair<int, int>;
-
+ 
 int main(int argc, char *argv[])
 {
+    int N, A, B;
+    std::cin >> N >> A >> B;
+
+    int max = 0;
+    int min = 0;
+    max = std::min(A, B);
+    if (N < A+B) {
+        min = A+B-N;
+    } else {
+        min = 0;
+    }
+
+    std::cout << max << " " << min << std::endl;
     return 0;
 }
