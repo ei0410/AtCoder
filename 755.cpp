212b58d555ec9784c0bf213f0256cb379d574cc4
ei <chronolavos0124@gmail.com>
2019-03-14 19:21:02 +0900

    2019_03_14

--- 755.cpp
+++ 755.cpp
@@ -1,35 +1,70 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> 
+ 
+#define INF INT_MAX
+#define MOD 1000000007
+#define PI  acos(-1)
+ 
+using ll  = long long;
+using ull = unsigned long long;
+using Pii = std::pair<int, int>;
 
-int main(int argc, char *argv[])
-{
-    int N;
-    std::cin >> N;
-    std::string s = std::to_string(N);
+ll count = 0;
+ll N;
 
-    if (s.size() <= 2) {
-        std::cout << 0 << std::endl;
-    } else if (s.size() == 3) {
-        int value = 0;
-        if (N < 357) {
-            value = 0;
-        } else if (N < 375) {
-            value = 1;
-        } else if (N < 537) {
-            value = 2;
-        } else if (N < 573) {
-            value = 3;
-        } else if (N < 735) {
-            value = 4;
-        } else if (N < 753) {
-            value = 5;
-        } else {
-            value = 6;
-        }
-        std::cout << value << std::endl;
-    } else {
-        int ans    = 0;
-        std::cout << ans << std::endl;
+void dfs(ll depth, ll n) {
+    if (n > N) {
+        return;
     }
 
+    ll tmp3 = n;
+    bool tmp3flag = false;
+    while(tmp3 > 0) {
+        if (tmp3 % 10 == 3) {
+            tmp3flag = true;
+            break;
+        }
+        tmp3 /= 10;
+    }
+    ll tmp5 = n;
+    bool tmp5flag = false;
+    while(tmp5 > 0) {
+        if (tmp5 % 10 == 5) {
+            tmp5flag = true;
+            break;
+        }
+        tmp5 /= 10;
+    }
+    ll tmp7 = n;
+    bool tmp7flag = false;
+    while(tmp7 > 0) {
+        if (tmp7 % 10 == 7) {
+            tmp7flag = true;
+            break;
+        }
+        tmp7 /= 10;
+    }
+
+    if (tmp3flag && tmp5flag && tmp7flag) {
+        count++;
+    }
+    if (depth == 0) {
+        depth++;
+        dfs(depth, 3);
+        dfs(depth, 5);
+        dfs(depth, 7);
+    } else {
+        dfs(depth+1, n+std::pow(10, depth)*3); 
+        dfs(depth+1, n+std::pow(10, depth)*5); 
+        dfs(depth+1, n+std::pow(10, depth)*7); 
+    }
+}
+ 
+int main(int argc, char *argv[])
+{
+    std::cin >> N;
+
+    dfs(0, 0);
+
+    std::cout << count << std::endl;
     return 0;
 }
